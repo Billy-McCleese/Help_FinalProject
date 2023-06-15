f5bd5fcbbf39c7577ae5f45882ec673ef98cdc82
Billy-McCleese <billymccleese@gmail.com>
2023-06-14 21:47:38 -0400

    added search by city/state for external api

--- Help_FinalProject/Controllers/MLSAPI.cs
+++ Help_FinalProject/Controllers/MLSAPI.cs
@@ -1,52 +1,47 @@
 ﻿using System.Net.Http;
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Text.Json;
 
-namespace Help_FinalProject.Controllers
+[ApiController]
+[Route("api/[controller]")]
+public class RealEstateController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class RealEstateController : ControllerBase
+    private readonly HttpClient _client;
+
+    public RealEstateController()
     {
+        _client = new HttpClient();
+        _client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "8e83093fd5mshda833beb7946c42p1b1ebbjsn0333ad489400");
+        _client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "us-real-estate.p.rapidapi.com");
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetRentalDataByZip(string zipcode, int offset = 1, int limit = 10, string sort = "lowest_price")
+    [HttpGet("rent")]
+    public async Task<ActionResult> GetRealEstateForRent(string city, string state)
+    {
+        var requestUri = $"https://us-real-estate.p.rapidapi.com/v2/for-rent?city={city}&state_code={state}&location=48278&limit=10&offset=0&sort=lowest_price&price_min=1000&price_max=3000&beds_min=1&beds_max=5&baths_min=1&baths_max=5&property_type=apartment&expand_search_radius=25&include_nearby_areas_slug_id=Union-City_NJ%2CHoward-Beach_NY&home_size_min=500&home_size_max=3000&in_unit_features=central_air&community_ammenities=garage_1_or_more&cats_ok=true&dogs_ok=true";
+
+        using (var response = await _client.GetAsync(requestUri))
         {
-            using (var client = new HttpClient())
-            {
-                var request = new HttpRequestMessage
-                {
-                    Method = HttpMethod.Get,
-                    RequestUri = new Uri($"https://us-real-estate.p.rapidapi.com/v2/for-rent-by-zipcode?zipcode={zipcode}&limit={limit}&offset={offset}&sort={sort}"),
-                    Headers =
-                    {
-                        { "X-RapidAPI-Key", "8e83093fd5mshda833beb7946c42p1b1ebbjsn0333ad489400" },
-                        { "X-RapidAPI-Host", "us-real-estate.p.rapidapi.com" },
-                    },
-                };
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
 
-                try
-                {
-                    using (var response = await client.SendAsync(request))
-                    {
-                        response.EnsureSuccessStatusCode();
-                        var responseBody = await response.Content.ReadAsStringAsync();
-                        var options = new JsonSerializerOptions
-                        {
-                            PropertyNameCaseInsensitive = true
-                        };
-                        var jsonData = JsonSerializer.Deserialize<object>(responseBody, options);
-
-                        return Ok(jsonData);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-            }
+            return Content(body, "application/json"); // Return the response as JSON
         }
+    }
 
+    [HttpGet("rent-by-zipcode")]
+    public async Task<ActionResult> GetRealEstateForRentByZipCode(string zipcode, int limit = 10, int offset = 1, string sort = "lowest_price")
+    {
+        var requestUri = $"https://us-real-estate.p.rapidapi.com/v2/for-rent-by-zipcode?zipcode={zipcode}&limit={limit}&offset={offset}&sort={sort}";
+
+        using (var response = await _client.GetAsync(requestUri))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+
+            return Content(body, "application/json"); // Return the response as JSON
+        }
     }
 }
